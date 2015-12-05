7913e32256f131fdc73e1d30bbf135997d23e9db
briandowling <briandowling@Brians-MacBook-Pro.local>
2015-12-05 12:47:17 -0500

    Final Project

---
+++ script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+
+document.getElementById("oolong").style.font = "italic bold 23px times new roman";
+document.getElementById("oolong").style.color = "#ff4d4d";
+
+
+
+ }
+
+function myFunctions() {
+
+
+document.getElementById("assam").style.font = "italic bold 25px arial";
+document.getElementById("assam").style.color = "#ffff4d";
+
+
+ }
